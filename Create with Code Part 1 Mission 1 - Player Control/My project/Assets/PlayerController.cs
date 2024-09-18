dec40afba4470d7d1b4dc4a4e77e2297f121c3da
rmcc83 <156690440+rmcc83@users.noreply.github.com>
2024-09-18 15:14:22 +0100

    End of 1.3

--- Create with Code Part 1 Mission 1 - Player Control/My project/Assets/PlayerController.cs
+++ Create with Code Part 1 Mission 1 - Player Control/My project/Assets/PlayerController.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+
+    public float speed = 5.0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +18,6 @@ public class PlayerController : MonoBehaviour
     {
         // Move the vehicle forward
 
-        transform.Translate(Vector3.forward * Time.deltaTime * 20);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
     }
 }
