0cdb0ea094a76374d684dd0482d275c28540773a
rmcc83 <156690440+rmcc83@users.noreply.github.com>
2024-09-18 15:59:49 +0100

    Rotate propellor

---
+++ Create with Code Part 1 Mission 1 - Player Control/My project/Assets/SpinPropellor.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinPropellor : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // rotate propellor around axis, 10 degrees per frame
+
+        transform.Rotate(0, 0, 10);
+    }
+}
