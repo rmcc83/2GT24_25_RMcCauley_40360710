22d49c854d3557e6cda56776a88a7b46f32a73e1
rmcc83 <156690440+rmcc83@users.noreply.github.com>
2024-09-18 15:55:48 +0100

    Camera follow fixed

--- Create with Code Part 1 Mission 1 - Player Control/My project/Assets/Challenge 1/Scripts/FollowPlayerX.cs
+++ Create with Code Part 1 Mission 1 - Player Control/My project/Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class FollowPlayerX : MonoBehaviour
 {
     public GameObject plane;
-    private Vector3 offset;
+    private Vector3 offset = new Vector3(33, 0, 0);
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +16,7 @@ public class FollowPlayerX : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Camera follows plane but is offset
         transform.position = plane.transform.position + offset;
     }
 }
