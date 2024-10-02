d1f3fac71d9d3f5b51f00fb92e456928d4fd0781
rmcc83 <156690440+rmcc83@users.noreply.github.com>
2024-10-02 19:12:03 +0100

    Create with code end 2.2

--- Create with Code Part 1 Mission 2 - Basic Gameplay/Animals/Assets/Scripts/PlayerController.cs
+++ Create with Code Part 1 Mission 2 - Basic Gameplay/Animals/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
     public float horizontalInput;
     public float speed = 10.0f;
     public float xRange = 10;
+    public GameObject projectilePrefab;
 
     // Start is called before the first frame update
     void Start()
@@ -21,15 +22,22 @@ public class PlayerController : MonoBehaviour
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
 
         //keep player in bounds
-        if (transform.position.x > xRange) 
-        {    
-            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);    
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
 
         if (transform.position.x < -xRange)
-        { 
+        {
             transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
 
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //launch a projectile from the player
+
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
         }
     }
 }
