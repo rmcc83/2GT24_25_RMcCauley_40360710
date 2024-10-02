eeb1a93e5ee91f335bc5d632b72d92db2bfba140
rmcc83 <156690440+rmcc83@users.noreply.github.com>
2024-10-02 22:17:35 +0100

    Create with code End 2.4

--- Create with Code Part 1 Mission 2 - Basic Gameplay/Animals/Assets/Scripts/DestroyOutOfBounds.cs
+++ Create with Code Part 1 Mission 2 - Basic Gameplay/Animals/Assets/Scripts/DestroyOutOfBounds.cs
@@ -24,6 +24,7 @@ public class DestroyOutOfBounds : MonoBehaviour
         else if (transform.position.z < lowerBound)
         {
             Destroy(gameObject);
+            Debug.Log("Game Over");
         }
     }
 }
