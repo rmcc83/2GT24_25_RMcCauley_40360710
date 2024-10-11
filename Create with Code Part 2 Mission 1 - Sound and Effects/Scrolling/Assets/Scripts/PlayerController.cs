01327ac3025b7846a26a91799248efa924f5e72c
rmcc83 <156690440+rmcc83@users.noreply.github.com>
2024-10-11 14:32:19 +0100

    Create with code - end of 3.3

--- Create with Code Part 2 Mission 1 - Sound and Effects/Scrolling/Assets/Scripts/PlayerController.cs
+++ Create with Code Part 2 Mission 1 - Sound and Effects/Scrolling/Assets/Scripts/PlayerController.cs
@@ -9,22 +9,25 @@ public class PlayerController : MonoBehaviour
     public float gravityModifier;
     public bool isOnGround = true;
     public bool gameOver;
+    private Animator playerAnim;
 
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
         Physics.gravity *= gravityModifier;
+        playerAnim = GetComponent<Animator>();
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyUp(KeyCode.Space) && isOnGround == true) 
+        if (Input.GetKeyUp(KeyCode.Space) && isOnGround == true && !gameOver) 
         {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
+            playerAnim.SetTrigger("Jump_trig");
 
         }
     }
@@ -40,6 +43,8 @@ public class PlayerController : MonoBehaviour
         else if (collision.gameObject.CompareTag("Obstacle")) 
         {
             gameOver = true;
+            playerAnim.SetBool("Death_b", true);
+            playerAnim.SetInteger("DeathType_int", 1);
             Debug.Log ("Game Over");
         
         }
