ec2bda0c23dce79c473ba97ea1f8f5c26cd6fabf
rmcc83 <156690440+rmcc83@users.noreply.github.com>
2024-10-31 16:22:45 +0000

    Create with Code 4 - end of 4.3

--- Create with Code Part 2 Mission 2 - Gameplay Mechanics/Unit 4/Assets/Scripts/PlayerController.cs
+++ Create with Code Part 2 Mission 2 - Gameplay Mechanics/Unit 4/Assets/Scripts/PlayerController.cs
@@ -6,8 +6,12 @@ public class PlayerController : MonoBehaviour
 {
 
     public float speed = 5.0f;
+    private float powerUpStrength = 15.0f;
     private Rigidbody playerRb;
     private GameObject focalPoint;
+    public bool hasPowerUp;
+    public GameObject powerUpIndicator;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,5 +24,37 @@ public class PlayerController : MonoBehaviour
     {
         float forwardInput = Input.GetAxis("Vertical");
         playerRb.AddForce(focalPoint.transform.forward * speed * forwardInput);
+        powerUpIndicator.transform.position = transform.position + new Vector3(0, -0.5f, 0);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("PowerUp"))
+        {
+            hasPowerUp = true;
+            powerUpIndicator.gameObject.SetActive(true);
+            Destroy(other.gameObject);
+            StartCoroutine(PowerUpCountdownRoutine());
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy") && hasPowerUp) 
+        {
+            Rigidbody enemyRigidbody = collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer = (collision.gameObject.transform.position - transform.position);
+
+            Debug.Log("Collided with" + collision.gameObject.name + "with PowerUp set to" + hasPowerUp);
+            enemyRigidbody.AddForce(awayFromPlayer* powerUpStrength, ForceMode.Impulse);
+        }
+    }
+
+    IEnumerator PowerUpCountdownRoutine() 
+    {
+        yield return new WaitForSeconds(7);
+        hasPowerUp = false;
+        powerUpIndicator.gameObject.SetActive(false);
+    
     }
 }
