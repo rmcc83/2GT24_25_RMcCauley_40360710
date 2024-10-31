b17de2ca052645130c2a712847328a0b63f5474b
rmcc83 <156690440+rmcc83@users.noreply.github.com>
2024-10-31 16:39:10 +0000

    Create with code 4 - end 4.4

--- Create with Code Part 2 Mission 2 - Gameplay Mechanics/Unit 4/Assets/Scripts/SpawnManager.cs
+++ Create with Code Part 2 Mission 2 - Gameplay Mechanics/Unit 4/Assets/Scripts/SpawnManager.cs
@@ -6,19 +6,30 @@ public class SpawnManager : MonoBehaviour
 {
 
     public GameObject enemyPrefab;
+    public GameObject powerUpPrefab;
     private float spawnRange = 9;
+    public int enemyCount;
+    public int waveNumber = 1;
 
     // Start is called before the first frame update
     void Start()
     {
-       
-        Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+        Instantiate(powerUpPrefab, GenerateSpawnPosition(), powerUpPrefab.transform.rotation);
+        SpawnEnemyWave(waveNumber);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        enemyCount = FindObjectsOfType<Enemy>().Length;
+        if (enemyCount == 0) 
+        {
+            Instantiate(powerUpPrefab, GenerateSpawnPosition(), powerUpPrefab.transform.rotation);
+            waveNumber ++;
+            SpawnEnemyWave(waveNumber);
+
+        }
     }
 
     private Vector3 GenerateSpawnPosition() 
@@ -27,5 +38,15 @@ public class SpawnManager : MonoBehaviour
         float spawnposZ = Random.Range(-spawnRange, spawnRange);
         Vector3 randomPos = new Vector3(spawnPosX, 0, spawnposZ);
         return randomPos;
+    }
+
+    void SpawnEnemyWave(int enemiesToSpawn) 
+    { 
+        for (int i = 0; i < enemiesToSpawn; i++) 
+        {
+            Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+
+        }
+          
     }
 }
