401b8f595bb3d8d4f7c2ed9223252d5a6c5320e7
rmcc83 <156690440+rmcc83@users.noreply.github.com>
2024-11-26 15:59:17 +0000

    Project - more highscore fireworks adjustment & refactor of player select button code

---
+++ Project/Bacteria Blast - Sam & Ella in Space/Assets/Scripts/ButtonNames.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+
+public class ButtonNames : MonoBehaviour
+{
+    public GameManager gameManager;
+    public TextMeshProUGUI player1Name;
+    public TextMeshProUGUI player2Name;
+    public TextMeshProUGUI player3Name;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        Buttons();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void Buttons()
+    {
+        player1Name.text = PlayerPrefs.GetString("Player1Name", "Player 1");
+        player2Name.text = PlayerPrefs.GetString("Player2Name", "Player 2");
+        player3Name.text = PlayerPrefs.GetString("Player3Name", "Player 3");
+    }
+}
--- Project/Bacteria Blast - Sam & Ella in Space/Assets/Scripts/PlayerSelect.cs
+++ Project/Bacteria Blast - Sam & Ella in Space/Assets/Scripts/PlayerSelect.cs
@@ -8,59 +8,46 @@ using TMPro;
 public class PlayerSelect : MonoBehaviour
 {
     public GameManager gameManager;
-    public TextMeshProUGUI player1Name;
-    public TextMeshProUGUI player2Name;
-    public TextMeshProUGUI player3Name;
-
+    private Button button;
+    public int profile;
 
     void Start()
     {
         // At start, method is run to retrieve names from player prefs & display them on the buttons
+        button = GetComponent<Button>();
+        button.onClick.AddListener(SetProfile);
         gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
-        ButtonNames();
 
     }
 
-    // selecting first button  will load profile 1
-    public void Button1()
+    public void SetProfile() 
     {
-        PlayerPrefs.SetInt("CurrentProfile", 1);
-        gameManager.player1 = true;
-        gameManager.player2 = false;
-        gameManager.player3 = false;
-
-
+        switch (profile) 
+        {
+            case 1:
+                PlayerPrefs.SetInt("CurrentProfile", 1);
+                gameManager.player1 = true;
+                gameManager.player2 = false;
+                gameManager.player3 = false;
+                gameManager.Load();
+                break;
+            case 2:
+                PlayerPrefs.SetInt("CurrentProfile", 3);
+                gameManager.player1 = false;
+                gameManager.player2 = true;
+                gameManager.player3 = false;
+                gameManager.Load();
+                break;
+            case 3: 
+                PlayerPrefs.SetInt("CurrentProfile", 3);
+                gameManager.player1 = false;
+                gameManager.player2 = false;
+                gameManager.player3 = true;
+                gameManager.Load();
+                break;
+        }
+       
     }
-
-    // selecting second button will load profile 2
-    public void Button2()
-    {
-        PlayerPrefs.SetInt("CurrentProfile", 2);
-        gameManager.player1 = false;
-        gameManager.player2 = true;
-        gameManager.player3 = false;
-
-    }
-
-    // selecting third button will load profile 3
-    public void Button3()
-    {
-        PlayerPrefs.SetInt("CurrentProfile", 3);
-        gameManager.player1 = false;
-        gameManager.player2 = false;
-        gameManager.player3 = true;
-
-    }
-
-    // retrieves names from player prefs and displays them on the buttons; if nothing saved in playerprefs, uses default of player 1/2/3
-    public void ButtonNames()
-    {
-        player1Name.text = PlayerPrefs.GetString("Player1Name", "Player 1");
-        player2Name.text = PlayerPrefs.GetString("Player2Name", "Player 2");
-        player3Name.text = PlayerPrefs.GetString("Player3Name", "Player 3");
-
-    }
-
 
 
 }
