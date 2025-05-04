b893183a2e9b0e33b5baa4ecba6b8382838fa9a3
rmcc83 <156690440+rmcc83@users.noreply.github.com>
2025-05-04 21:25:37 +0100

    Character bio audio update
    Script to pause/restart music when voice is playing, and also to disable/re-enable audio play button

---
+++ Project/Bacteria Blast - Sam & Ella in Space/Assets/Scripts/CharacterBiosAudio.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterBiosAudio : MonoBehaviour
+{
+    public AudioSource characterAudioSource;
+    public AudioSource musicAudioSource1;
+    public Button samButton;
+    public Button ellaButton;
+    public Button redButton;
+    public Button blueButton;
+    public Button purpleButton;
+    public Button virusButton;
+    public Button fuelButton;
+    public Button weaponButton;
+    public Button repairButton;
+    public Button asteroidButton;
+    public Button spaceshipButton;
+    public Button escapePod;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MusicAndButton();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MusicAndButton();
+
+    }
+
+    public void MusicAndButton() 
+    {
+        if (characterAudioSource.isPlaying)
+        {
+            musicAudioSource1.Pause();
+            samButton.interactable = false;
+            ellaButton.interactable = false;
+            redButton.interactable = false;
+            blueButton.interactable = false;
+            purpleButton.interactable = false;
+            virusButton.interactable = false;
+            fuelButton.interactable = false;
+            weaponButton.interactable = false;
+            repairButton.interactable = false;
+            asteroidButton.interactable = false;
+            spaceshipButton.interactable = false;
+            escapePod.interactable = false;
+
+        }
+
+        if (!characterAudioSource.isPlaying)
+        {
+            musicAudioSource1.UnPause();
+            samButton.interactable = true;
+            ellaButton.interactable = true;
+            redButton.interactable = true;
+            blueButton.interactable = true;
+            purpleButton.interactable = true;
+            virusButton.interactable = true;
+            fuelButton.interactable = true;
+            weaponButton.interactable = true;
+            repairButton.interactable = true;
+            asteroidButton.interactable = true;
+            spaceshipButton.interactable = true;
+            escapePod.interactable = true;
+
+        }
+
+    }
+
+}
